572d6aabb577030bf06a448bc9a3f503121d2f36
ki.im <ki.im@whatap.io>
2022-10-24 09:01:13 +0900

    modify-sidebar
    Former-commit-id: f4642d25d885ff858b19ec33499a818d3de7d55f

--- sidebars.js
+++ sidebars.js
@@ -57,7 +57,6 @@ const sidebars = {
           ]
         },
       ],
-      
     },
   ],
   apmSidebar: [
@@ -78,12 +77,13 @@ const sidebars = {
           collapsed: true,
           link: {
             type: 'doc',
-            id: 'apm/intro-java'
+            id: 'apm/java-intro'
           },
           items: [
             {
               type: 'doc',
-              id: 'apm/supported-spec'
+              label: '지원 환경',
+              id: 'apm/java-supported-spec',
             },
             {
               type: 'category',
@@ -109,33 +109,64 @@ const sidebars = {
                 'apm/add-jvm-opt/elastic-beanstalk',
                 'apm/add-jvm-opt/docker',
                 'apm/add-jvm-opt/batch-app',
+                'apm/add-jvm-opt/java-under-ver',
               ]
             },
+            'apm/java-after-install-agent',
             {
               type: 'doc',
-              label: '설치 시 문제 해결',
+              label: '설치 문제 해결',
               id: 'apm/java-agent-troubleshooting'
             },
             {
-              type: 'doc',
-              label: '에이전트 설정',
-              id: 'apm/java-set-agent'
+              type: 'category',
+              label: '설정하기',
+              collapsible: true,
+              collapsed: true,
+              items: [
+                'apm/java-set-agent',
+                {
+                  type: 'doc',
+                  label: '에이전트 로그',
+                  id: 'apm/java-agent-log'
+                },
+                {
+                  type: 'doc',
+                  label: '에이전트 이름 식별',
+                  id: 'apm/java-agent-name'
+                },
+                {
+                  type: 'category',
+                  label: '에이전트 플러그인',
+                  items: [
+                    {
+                      type: 'doc',
+                      label: '스크립트 플러그인',
+                      id: 'apm/java-script-plugin'
+                    },
+                    {
+                      type: 'doc',
+                      label: '위빙 플러그인',
+                      id: 'apm/java-weaving-plugin'
+                    },
+                  ]
+                },
+                'apm/java-agent-adv-settings'
+              ]
             },
             {
-              type: 'doc',
-              label: '에이전트 설치 파일 구성',
-              id: 'apm/java-agent-files'
-            },
-            {
-              type: 'doc',
-              label: '에이전트 로그',
-              id: 'apm/java-agent-log'
-            }
-            ,
-            {
-              type: 'doc',
-              label: '에이전트 이름 식별',
-              id: 'apm/java-agent-name'
+              type: 'category',
+              label: '관리하기',
+              items: [
+                {
+                  type: 'doc',
+                  label: '에이전트 업데이트 및 삭제',
+                  id: 'apm/java-update-agent'
+                },
+                'apm/java-msa-group',
+                'apm/java-aes-256-encryption',
+                'apm/java-scale-in-auto-delete',
+              ]
             }
           ]
         },
@@ -173,6 +204,51 @@ const sidebars = {
               label: '설치 시 문제 해결',
               id: 'apm/php-agent-troubleshooting'
             }
+          ]
+        },
+        {
+          type: 'category',
+          label: 'Node.js 모니터링',
+          collapsible: true,
+          collapsed: true,
+          link: {
+            type: 'doc',
+            id: 'apm/nodejs-intro'
+          },
+          items: [
+            {
+              type: 'doc',
+              label: '지원 환경',
+              id: 'apm/nodejs-supported-spec'
+            },
+            {
+              type: 'category',
+              label: '에이전트 설치',
+              collapsible: true,
+              collapsed: true,
+              link: {
+                type: 'doc',
+                id: 'apm/nodejs-install-agent'
+              },
+              items: [
+                
+              ]
+            }
+          ]
+        },
+        {
+          type: 'category',
+          label: '주요 메뉴 알아보기',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            'apm/apm-dashboard',
+            'apm/apm-transaction',
+            'apm/apm-topology',
+            'apm/apm-metrics',
+            'apm/apm-analyze',
+            'apm/apm-set-notics',
+            'apm/apm-openapi'
           ]
         }
       ]
