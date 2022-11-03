8834a113f01256c645502f144cf70749945a87ea
ki.im <ki.im@whatap.io>
2022-11-04 08:55:53 +0900

    메타 정보 수정
    Former-commit-id: b9ccebc52950a9a86a8128dc077ca11427fa0489

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'WhaTap', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  projectName: 'WhaTap Docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
