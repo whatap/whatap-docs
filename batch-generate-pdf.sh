7b3552ffd5e39ff205b07821c9315f255e95f856
ki.im <ki.im@whatap.io>
2022-12-29 17:28:42 +0900

    update batch
    Former-commit-id: 865cf3d92cea607bd1b3ba0beaf23784ef1e8062

--- batch-generate-pdf.sh
+++ batch-generate-pdf.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/apm/application-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-apm-monitoring.pdf
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/server/server-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-server-monitoring.pdf
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/db/db-monitoring-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-db-monitoring.pdf
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/kubernetes/kubernetes-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-kubernetes-monitoring.pdf
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/log/log-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-log-monitoring.pdf
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/url/url-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-url-monitoring.pdf
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/manage --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-manage.pdf
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/reference --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-reference.pdf
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/apm/application-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-apm-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/server/server-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-server-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/db/db-monitoring-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-db-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/kubernetes/kubernetes-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-kubernetes-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/log/log-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-log-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/url/url-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-url-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/manage --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-manage.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/reference --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-reference.pdf --include-index
 # npx docusaurus-prince-pdf -u http://localhost:4000/docs/integration/integ-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/integration-monitoring.pdf
