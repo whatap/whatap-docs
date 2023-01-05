093a4c852810564f907453a808f56e183dea96fb
ki.im <ki.im@whatap.io>
2023-01-06 08:56:44 +0900

    update settings
    Former-commit-id: 324d658e6fc4b925b0924afa8948565bbad17bbf

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -102,6 +102,14 @@ const config = {
           customCss: require.resolve('./src/css/custom.css'),
         },
       }),
+      {
+        sitemap: {
+          changefreq: 'weekly',
+          priority: 0.5,
+          ignorePatterns: ['/tags/**'],
+          filename: 'sitemap.xml',
+        }
+      }
     ],
   ],
   themeConfig:
