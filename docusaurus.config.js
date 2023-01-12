22a69e31e00fbe93be45beb8b56f56a185eaf8e5
ki.im <ki.im@whatap.io>
2023-01-12 09:01:31 +0900

    update css & config.js
    Former-commit-id: 50adbe9e7b52ae84ff58bd89a852b768582358c2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,54 +33,54 @@ const config = {
     // require.resolve('./static/js/channel_ko.js')
   ],
   plugins: [
-    [
-      '@docusaurus/plugin-client-redirects',
-      {
-        fromExtensions: ['html', 'htm'],
-        redirects: [
-          {
-            to: '/url/url-intro',
-            from: '/use_guide/url_monitoring/intro'
-          },
-          {
-            to: '/url/url-intro',
-            from: '/use_guide/url_monitoring/intro.html'
-          },
-          {
-            to: '/server/server-intro',
-            from: '/whatap_guide/install_agent/server/support_env.html'
-          },
-          {
-            to: '/server/server-intro',
-            from: '/whatap_guide/install_agent/server/support_env'
-          },
-          {
-            to: '/kubernetes/kubernetes-intro',
-            from: '/whatap_guide/install_agent/kubernetes/install.html',
-          },
-          {
-            to: '/kubernetes/kubernetes-intro',
-            from: '/whatap_guide/install_agent/kubernetes/install',
-          },
-          {
-            to: '/mysql/install-db-agent',
-            from: '/kr/agent_mysql'
-          },
-          {
-            to: '/mssql/install-db-agent',
-            from: '/kr/agent_mssql'
-          },
-          {
-            to: '/tibero/install-db-agent',
-            from: '/kr/agent_tibero'
-          },
-          {
-            to: '/postgresql/install-db-agent',
-            from: '/kr/agent_postgresql'
-          }
-        ]
-      }
-    ],
+    // [
+    //   '@docusaurus/plugin-client-redirects',
+    //   {
+    //     fromExtensions: ['html', 'htm'],
+    //     redirects: [
+    //       {
+    //         to: '/url/url-intro',
+    //         from: '/use_guide/url_monitoring/intro'
+    //       },
+    //       {
+    //         to: '/url/url-intro',
+    //         from: '/use_guide/url_monitoring/intro.html'
+    //       },
+    //       {
+    //         to: '/server/server-intro',
+    //         from: '/whatap_guide/install_agent/server/support_env.html'
+    //       },
+    //       {
+    //         to: '/server/server-intro',
+    //         from: '/whatap_guide/install_agent/server/support_env'
+    //       },
+    //       {
+    //         to: '/kubernetes/kubernetes-intro',
+    //         from: '/whatap_guide/install_agent/kubernetes/install.html',
+    //       },
+    //       {
+    //         to: '/kubernetes/kubernetes-intro',
+    //         from: '/whatap_guide/install_agent/kubernetes/install',
+    //       },
+    //       {
+    //         to: '/mysql/install-db-agent',
+    //         from: '/kr/agent_mysql'
+    //       },
+    //       {
+    //         to: '/mssql/install-db-agent',
+    //         from: '/kr/agent_mssql'
+    //       },
+    //       {
+    //         to: '/tibero/install-db-agent',
+    //         from: '/kr/agent_tibero'
+    //       },
+    //       {
+    //         to: '/postgresql/install-db-agent',
+    //         from: '/kr/agent_postgresql'
+    //       }
+    //     ]
+    //   }
+    // ],
     [
       '@docusaurus/plugin-google-gtag',
       {
