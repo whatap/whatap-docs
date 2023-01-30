3f669eed7000204209eb94c9eb721c7f6c36ac1f
ki.im <ki.im@whatap.io>
2023-01-30 09:51:48 +0900

    update pdfs
    Former-commit-id: 8ca2a0fb3686141aceaf787e93c32f1d5419be02

--- batch-generate-pdf.sh
+++ batch-generate-pdf.sh
@@ -5,7 +5,7 @@ npx docusaurus-prince-pdf -u http://localhost:4000/docs/nodejs/introduction --pr
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/python/introduction --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-apm-python-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/dotnet/introduction --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-apm-dotnet-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/golang/introduction --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-apm-golang-monitoring.pdf --include-index
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/server/server-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-server-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/server/introduction --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-server-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/postgresql/monitoring-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-db-postgresql-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/oracle/monitoring-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-db-oracle-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/mysql/monitoring-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-db-mysql-monitoring.pdf --include-index
@@ -15,10 +15,11 @@ npx docusaurus-prince-pdf -u http://localhost:4000/docs/cubrid/monitoring-intro 
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/altibase/monitoring-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-db-altibase-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/redis/monitoring-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-db-redis-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/mongodb/monitoring-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-db-mongodb-monitoring.pdf --include-index
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/kubernetes/kubernetes-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-kubernetes-monitoring.pdf --include-index
-npx docusaurus-prince-pdf -u http://localhost:4000/docs/log/log-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-log-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/kubernetes/introduction --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-kubernetes-monitoring.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/log/introduction --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-log-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/url/url-intro --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-url-monitoring.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/manage --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-manage.pdf --include-index
 npx docusaurus-prince-pdf -u http://localhost:4000/docs/reference --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-reference.pdf --include-index
+npx docusaurus-prince-pdf -u http://localhost:4000/docs/extensions --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-extensions.pdf --include-index
 
 # npx docusaurus-prince-pdf -u http://localhost:4000/docs/best-practice-guides/about-apm-dashboard --prince-args="--page-size='a4' --page-margin='10mm'" -o ./pdf/whatap-best-practice-guides-about-apm-dashboard.pdf --include-index
