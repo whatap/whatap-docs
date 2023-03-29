67a041eb1fec468998b4f98fb96b08367086ef32
whatap-docs <kj.im@whatap.io>
2023-03-29 18:31:08 +0900

    update config.js
    Former-commit-id: 1158405846f3065d43945777635be9780d920c6a

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
         highlightSearchTermsOnTargetPage: true,
         explicitSearchResultPath: true,
         searchResultContextMaxLength: 50,
-        ignoreFiles: [/wip/, /on-prem/, /common-items/, /mysql-rds/, /aws-log-p/],
+        ignoreFiles: [/wip/, /on-prem/, /common-items/, /mysql-rds/, /aws-log-p/, /release-notes\/preview/],
       }
     ],
     [
