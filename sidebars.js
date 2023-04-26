a180c24e0aa7df1513f6989b74f841759c22a9db
ysjo <ysjo@whatap.io>
2023-04-26 10:55:05 +0900

    update sdb
    Former-commit-id: d2ff00becae36e640f70dd33699970bdfe64545c

--- sidebars.js
+++ sidebars.js
@@ -4945,7 +4945,7 @@ const sidebars = {
             'log/log-java',
             'log/log-php',
             'log/log-python',
-            // 'log/log-go',
+            'log/log-go',
             'log/log-server',
             'log/log-k8s',
           ],
