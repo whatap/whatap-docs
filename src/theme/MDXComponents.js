f16ffa7d701cafd961bb20e76f2a2392a7f95746
whatap-docs <kj.im@whatap.io>
2023-04-26 11:50:13 +0900

    update components
    Former-commit-id: ff9255235d0aa885c8e9db0c0dd4b8098dd020be

--- src/theme/MDXComponents.js
+++ src/theme/MDXComponents.js
@@ -8,10 +8,14 @@ import ProdImg from '@site/src/components/ChangeImgName.js';
 import Tabs from '@theme/Tabs';
 import TabItem from '@theme/TabItem';
 import DocCardList from '@theme/DocCardList';
+import Status from '@site/src/components/Highlight';
+import UI from '@site/src/components/UItext';
 
 export default {
   // 기본 mapping 재사용
   ...MDXComponents,
+  Status,
+  UI,
   InDoc,
   Xclude,
   TR,
