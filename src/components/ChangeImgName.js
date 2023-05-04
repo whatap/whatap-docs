5ab3c342beb39de6db889e3e1a526235b7387af1
whatap-docs <kj.im@whatap.io>
2023-05-04 15:20:37 +0900

    update components
    Former-commit-id: 690f6b23a0cbc519d49aec234405cd63b32e870e

--- src/components/ChangeImgName.js
+++ src/components/ChangeImgName.js
@@ -1,3 +1,21 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:35bbc9791252da0c544edad52a1fd9b72ce871f8c0ce8f0d8bb99b618a37725e
-size 589
+import React from 'react';
+import checkProduct from '@site/src/components/CheckProduct';
+
+export default function ChangeImgName({img, desc}) {
+    const product = checkProduct();
+    let fext = img.substr(img.lastIndexOf('.') + 1);
+    let fileName = img.replace('.' + fext, '');
+    let imgFilePath = '/img/' + fileName + product + '.' + fext;
+    const onErrorImg = (e) => {
+        e.target.src = '/img/' + img;
+    }
+    
+    return (
+        <p>
+            <img src={imgFilePath} 
+                alt={desc} 
+                onError={onErrorImg} 
+            />
+        </p>
+    );
+}
