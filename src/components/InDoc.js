da6c1539b263439f312de2352241eddad08a0f19
whatap-docs <kj.im@whatap.io>
2023-05-22 22:24:15 +0900

    update component for translation
    Former-commit-id: 324fe0f2041822fb2d7edb631a832553d64a1669

--- src/components/InDoc.js
+++ src/components/InDoc.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import MDXContents from '@theme-original/MDXContent';
 import {useLocation} from '@docusaurus/router';
-
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 export default function InDoc ({children, product}) {
+    const {
+        i18n: {currentLocale},
+    } = useDocusaurusContext();
     let prods;
     if (typeof product.split(",") !== undefined) {
         prods = product.split(",");
@@ -10,8 +13,12 @@ export default function InDoc ({children, product}) {
         prods.push(product);
     }
     let location = useLocation();
-    let myContents;
-    let cProd = location.pathname.split("/")[1];
+    let myContents, cProd;
+    if (currentLocale == 'ko') {
+        cProd = location.pathname.split("/")[1];
+    } else {
+        cProd = location.pathname.split("/")[2];
+    }
     let i, prod;
     
     for (i =0; i < prods.length; i ++) {
