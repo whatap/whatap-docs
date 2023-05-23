5efb3bc0a660fe1e2e78f40c11cd6b67b3df8700
whatap-docs <kj.im@whatap.io>
2023-05-23 22:28:31 +0900

    update config.js
    Former-commit-id: 8f926a67ab9f93cb03e2b434e20e1bfc7562daef

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -138,7 +138,7 @@ module.exports = Promise.resolve({
         },
         // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
         typesenseSearchParameters: {
-          highlight_affix_num_tokens: 400,
+          // highlight_affix_num_tokens: 150,
         },
         // Optional
         contextualSearch: true,
