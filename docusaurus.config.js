b72225c4966adfab32e7ee6fb9aa7c3a5650590c
whatap-docs <kj.im@whatap.io>
2023-06-12 16:47:29 +0900

    update typesense settings
    Former-commit-id: 5b324bbb9c0fe662fe0dcfd325018cedf7446fc1

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -129,12 +129,12 @@ module.exports = Promise.resolve({
         typesenseServerConfig: {
           nodes: [
             {
-              host: 'typesense.whatap.io',
+              host: 't2dvabr5xq7wn91gp-1.a1.typesense.net',
               port: 443,
               protocol: 'https',
             },
           ],
-          apiKey: 'BP9kCFC5bN3GkU3IIQFRqNM7kbsSbWYaE74TyUP9jxuquARY',
+          apiKey: 'VXa4VAOWbU1Z911QbsNioXVGu1YXLuYW',
         },
         // Optional: Typesense search parameters: https://typesense.org/docs/0.24.0/api/search.html#search-parameters
         typesenseSearchParameters: {
