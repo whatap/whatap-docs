84dd4f2dd46c9f8d155572cc92c8970e349e5e57
whatap-docs <kj.im@whatap.io>
2023-06-13 11:44:23 +0900

    update config.js
    Former-commit-id: 4571451f8c6e812c71a204dcdb44b401b5e7780c

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -133,9 +133,10 @@ module.exports = Promise.resolve({
         contextualSearch: true,
         // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
         externalUrlRegex: 'external\\.com|domain\\.com',
-        
         // Optional: Algolia search parameters
-        searchParameters: {},
+        searchParameters: {
+          
+        },
         // Optional: path for search page that enabled by default (`false` to disable it)
         searchPagePath: 'search',
         //... other Algolia params
