a552230482c2716c1864a558e98c29ea22c24437
whatap-docs <kj.im@whatap.io>
2023-06-28 17:59:18 +0900

    update ProdImg components
    Former-commit-id: 4984a6cd5ebd399d728ec222e79a11eb3d908fb9

--- src/components/ChangeImgName.js
+++ src/components/ChangeImgName.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import checkProduct from '@site/src/components/CheckProduct';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 export default function ChangeImgName({img, desc}) {
+    const {
+        i18n: {currentLocale},
+    } = useDocusaurusContext();
     const product = checkProduct();
     let fext = img.substr(img.lastIndexOf('.') + 1);
     let fileName = img.replace('.' + fext, '');
     let imgFilePath = useBaseUrl('/img/' + fileName + product + '.' + fext);
     
     function onError(e) {
-        console.log(img);
-        e.target.src = '/img/' + img;
+        if (currentLocale != 'ko') {
+            e.target.src = '/' + currentLocale + '/img/' + img;
+        } else {
+            e.target.src = '/img/' + img;
+        }
     }
     return (
         <p>
