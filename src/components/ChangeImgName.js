690ce1a76f2524ee6ea6a9cd580c85b833aea4e0
whatap-docs <kj.im@whatap.io>
2023-07-04 16:56:59 +0900

    update images & documents
    Former-commit-id: 2fa378e2f0d103ee7ddd2a61dcd4f6b4abd3d37f

--- src/components/ChangeImgName.js
+++ src/components/ChangeImgName.js
@@ -2,26 +2,32 @@ import React from 'react';
 import checkProduct from '@site/src/components/CheckProduct';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-export default function ChangeImgName({img, desc}) {
+export default function ChangeImgName({img, desc, className}) {
     const {
         i18n: {currentLocale},
     } = useDocusaurusContext();
     const product = checkProduct();
     let fext = img.substr(img.lastIndexOf('.') + 1);
     let fileName = img.replace('.' + fext, '');
-    let imgFilePath = useBaseUrl('/img/' + fileName + product + '.' + fext);
+    let imgFilePath;
+    if (currentLocale != 'ko') {
+        imgFilePath = useBaseUrl('/img/' + fileName + product + '-' + currentLocale + '.' + fext);
+    } else {
+        imgFilePath = useBaseUrl('/img/' + fileName + product + '.' + fext);
+    }
     
     function onError(e) {
         if (currentLocale != 'ko') {
-            e.target.src = '/' + currentLocale + '/img/' + img;
+            e.target.src = '/' + currentLocale + '/img/' + fileName + product + '.' + fext;
         } else {
-            e.target.src = '/img/' + img;
+            e.target.src = '/img/' + fileName + product + '.' + fext;
         }
     }
     return (
         <p>
             <img src={imgFilePath} 
                 alt={desc} 
+                class={className}
                 onError={(e) => onError(e)}
             />
         </p>
