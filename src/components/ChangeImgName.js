a398793df35aa74cfb2865ef3981fb76493034e4
whatap-docs <kj.im@whatap.io>
2023-07-07 16:11:21 +0900

    rollback component
    Former-commit-id: 24d26534744cea48cd1303dba2a12b113d28baf4

--- src/components/ChangeImgName.js
+++ src/components/ChangeImgName.js
@@ -1,32 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import checkProduct from '@site/src/components/CheckProduct';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 
 export default function ChangeImgName({img, desc, className}) {
-    const TransImage = ({ src, alt, className, locale, ...props }) => {
-        const [imageExists, setImageExists] = useState(true);
-        
-        useState(() => {
-            const img = new Image();
-            img.src = src;
-            img.onload = () => {
-                setImageExists(true);
-            };
-            img.onerror = () => {
-                setImageExists(false);
-            };
-        }, [src]);
-        
-        if (imageExists) {
-            return <img src={src} alt={alt} class={className} {...props} onError={(e) => onError(e)}/>;
-        } else {
-            let changesrc = src.replace('-' + locale, '');
-            return <img src={changesrc} alt={alt} class={className} {...props} onError={(e) => onError(e)}/>
-        }
-        
-    };
-
     const {
         i18n: {currentLocale},
     } = useDocusaurusContext();
@@ -39,17 +16,21 @@ export default function ChangeImgName({img, desc, className}) {
     } else {
         imgFilePath = useBaseUrl('/img/' + fileName + product + '.' + fext);
     }
-
+    
     function onError(e) {
         if (currentLocale != 'ko') {
-            e.target.src = '/' + currentLocale + '/img/' + fileName + '.' + fext;
+            e.target.src = '/' + currentLocale + '/img/' + fileName + product + '.' + fext;
         } else {
-            e.target.src = '/img/' + fileName + '.' + fext;
+            e.target.src = '/img/' + fileName + product + '.' + fext;
         }
     }
     return (
         <p>
-            <TransImage src={imgFilePath} alt={desc} className={className} locale={currentLocale}/>
+            <img src={imgFilePath} 
+                alt={desc} 
+                class={className}
+                onError={(e) => onError(e)}
+            />
         </p>
     );
 }
