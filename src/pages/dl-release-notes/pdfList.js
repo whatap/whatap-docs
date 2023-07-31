dc1e729c857ff33167130348e7983b7a181bbea1
whatap-docs <kj.im@whatap.io>
2023-07-31 11:37:42 +0900

    update

---
+++ src/pages/dl-release-notes/pdfList.js
@@ -0,0 +1,80 @@
+const SvpdfList = [
+    {
+        name: 'service-1.104.x',
+        url: '/pdf/release-notes/service-1.104.x.pdf',
+    },
+    {
+        name: 'service-1.102.x',
+        url: '/pdf/release-notes/service-1.102.x.pdf',
+    },
+    {
+        name: 'service-1.100.x',
+        url: '/pdf/release-notes/service-1.100.x.pdf',
+    },
+    {
+        name: 'service-1.98.x',
+        url: '/pdf/release-notes/service-1.98.x.pdf',
+    },
+    {
+        name: 'service-1.96.0',
+        url: '/pdf/release-notes/service-1.96.0.pdf',
+    },
+    {
+        name: 'service-1.94.0',
+        url: '/pdf/release-notes/service-1.94.0.pdf',
+    },
+    {
+        name: 'service-1.92.0',
+        url: '/pdf/release-notes/service-1.92.0.pdf',
+    },
+    {
+        name: 'service-1.90.0',
+        url: '/pdf/release-notes/service-1.90.0.pdf',
+    },
+    {
+        name: 'service-1.86.0',
+        url: '/pdf/release-notes/service-1.86.0.pdf',
+    },
+    {
+        name: 'service-1.84.0',
+        url: '/pdf/release-notes/service-1.84.0.pdf',
+    },
+    {
+        name: 'service-1.82.0',
+        url: '/pdf/release-notes/service-1.82.0.pdf',
+    },
+    {
+        name: 'service-1.80.0',
+        url: '/pdf/release-notes/service-1.80.0.pdf',
+    },
+    // 추가적인 PDF 파일들을 원하는 만큼 목록에 추가할 수 있습니다.
+];
+
+const javaList = [
+    {
+        name: 'java-agent-v2.2.12',
+        url: '/pdf/release-notes/java-agent-v2.2.12.pdf',
+    },
+    {
+        name: 'java-agent-v2.2.11',
+        url: '/pdf/release-notes/java-agent-v2.2.11.pdf',
+    },
+    {
+        name: 'java-agent-v2.2.10',
+        url: '/pdf/release-notes/java-agent-v2.2.10.pdf',
+    },
+    {
+        name: 'java-agent-v2.2.9',
+        url: '/pdf/release-notes/java-agent-v2.2.9.pdf',
+    },
+    {
+        name: 'java-agent-v2.2.8',
+        url: '/pdf/release-notes/java-agent-v2.2.8.pdf',
+    },
+    {
+        name: 'java-agent-v2.2.7',
+        url: '/pdf/release-notes/java-agent-v2.2.7.pdf',
+    },
+];
+
+export {SvpdfList, javaList}
---
+++ src/pages/dl-release-notes/service.js
@@ -0,0 +1,90 @@
+import React, { useState } from 'react';
+import { saveAs } from 'file-saver';
+import JSZip from 'jszip';
+import styles from './styles.module.css';
+
+const downloadFilesAsZip = (selectedFiles) => {
+    // JSZip을 사용하여 zip 파일 생성
+    const zip = new JSZip();
+    const promises = [];
+
+    // 선택한 파일들을 zip에 추가
+    selectedFiles.forEach((file) => {
+        const promise = fetch(file.url)
+        .then((response) => response.blob())
+        .then((blob) => {
+            zip.file(file.name + '.pdf', blob, { binary: true });
+        });
+        promises.push(promise);
+    });
+    // 모든 파일을 로드한 후에 zip으로 압축하고 다운로드
+    Promise.all(promises).then(() => {
+        zip.generateAsync({ type: 'blob'}).then((content) => {
+            saveAs(content, 'download.zip')
+        })
+    });
+};
+
+export default function PDFDownloads({h2title, typeName, pdfList}) {
+    const [selectedFiles, setSelectedFiles] = useState([]);
+
+    const handleCheckboxChange = (event, file) => {
+        if (event.target.checked) {
+            setSelectedFiles([...selectedFiles, file]);
+        } else {
+            setSelectedFiles(selectedFiles.filter((selectedFile) => selectedFile !== file));
+        }
+    };
+
+    const handleSelectAll = (e) => {
+        if (e.target.checked) {
+            setSelectedFiles(pdfList);
+        } else {
+            setSelectedFiles([]);
+        }
+    };
+
+    const handleDownload = () => {
+        if (selectedFiles.length === 1) {
+            // 선택한 파일이 1개일 경우 개별 파일 다운로드
+            saveAs(selectedFiles[0].url, selectedFiles[0].name + '.pdf');
+        } else if (selectedFiles.length > 1) {
+            // 선택한 파일이 2개 이상일 경우 zip 파일로 압축하여 다운로드
+            downloadFilesAsZip(selectedFiles);
+        }
+    };
+
+    return (
+        <>
+            <h2>{h2title}</h2>
+            <div>
+                <input
+                    type='checkbox'
+                    id='select-all'
+                    name='select-all'
+                    className={styles.inputBox}
+                    onChange={handleSelectAll}
+                    checked={selectedFiles.length === pdfList.length}
+                />
+                <label htmlFor='select-all'>Select All</label>
+            </div>
+            <div className={styles.dlList}>
+                {pdfList.map((pdf, index) => (
+                    <div key={index} className={styles.dlListItem}>
+                    <input
+                        type="checkbox"
+                        id={`${typeName}-${index}`}
+                        className={styles.inputBox}
+                        name={pdf.name}
+                        onChange={(event) => handleCheckboxChange(event, pdf)} checked={selectedFiles.includes(pdf)}
+                    />
+                    <label htmlFor={`${typeName}-${index}`}>{pdf.name}</label>
+                    </div>
+                ))}
+            </div>
+            <div className={styles.dlBtn}>
+                <button className={styles.dlButton} onClick={handleDownload}>Download</button>
+            </div>
+        </>
+    );
+};
