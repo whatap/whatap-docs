cc00d33189742c505e2be0ade4ee0c496f60a509
whatap-docs <kj.im@whatap.io>
2023-08-02 17:11:30 +0900

    update images, component, docs

--- src/components/LinkImage.js
+++ src/components/LinkImage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 
-export default function LinkImage({img, desc}) {
+export default function LinkImage({img, desc, className}) {
     let imgPath = useBaseUrl('/img/' + img);
     return (
         <img src={imgPath}
             alt={desc}
+            class={className}
         />
     )
 }
