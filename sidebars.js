2aa6fa6d79fc6f5fa7a62e6f092a2869b1470e88
whatap-docs <kj.im@whatap.io>
2023-08-03 14:49:52 +0900

    update images

--- sidebars.js
+++ sidebars.js
@@ -368,6 +368,7 @@ const sidebars = {
               },
               items: [
                 'php/dashboard',
+                'php/dashboard-hitmap-trace',
                 'php/dashboard-transactionmap',
                 'php/dashboard-active-transaction',
               ],
@@ -586,6 +587,7 @@ const sidebars = {
               },
               items: [
                 'nodejs/dashboard',
+                'nodejs/dashboard-hitmap-trace',
                 'nodejs/dashboard-transactionmap',
                 'nodejs/dashboard-active-transaction',
               ],
@@ -807,6 +809,7 @@ const sidebars = {
               },
               items: [
                 'python/dashboard',
+                'python/dashboard-hitmap-trace',
                 'python/dashboard-transactionmap',
                 'python/dashboard-active-transaction',
               ],
@@ -1011,6 +1014,7 @@ const sidebars = {
               },
               items: [
                 'dotnet/dashboard',
+                'dotnet/dashboard-hitmap-trace',
                 'dotnet/dashboard-transactionmap',
                 'dotnet/dashboard-active-transaction',
               ],
@@ -1204,6 +1208,7 @@ const sidebars = {
               },
               items: [
                 'golang/dashboard',
+                'golang/dashboard-hitmap-trace',
                 'golang/dashboard-transactionmap',
                 'golang/dashboard-active-transaction',
               ],
