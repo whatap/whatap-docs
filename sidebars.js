710190e2db653884055281e4bf8614d7c7d8e54f
whatap-docs <kj.im@whatap.io>
2023-08-07 09:06:47 +0900

    [개발] 쿠버네티스 에이전트 1.3.3 버전 배포 - 릴리스 노트

--- sidebars.js
+++ sidebars.js
@@ -3269,14 +3269,15 @@ const sidebars = {
               className: 'new',
               link: {
                 type: 'doc',
-                id: 'release-notes/k8s/k8s-1_3_2',
+                id: 'release-notes/k8s/k8s-1_3_3',
               },
               items: [
                 {
                   type: 'doc',
-                  id: 'release-notes/k8s/k8s-1_3_2',
+                  id: 'release-notes/k8s/k8s-1_3_3',
                   className: 'new',
                 },
+                'release-notes/k8s/k8s-1_3_2',
                 'release-notes/k8s/k8s-1_3_1',
                 'release-notes/k8s/k8s-1_2_9',
                 'release-notes/k8s/k8s-1_2_8',
@@ -3285,7 +3286,6 @@ const sidebars = {
                 'release-notes/k8s/k8s-1_2_5',
                 'release-notes/k8s/k8s-1_2_2',
                 'release-notes/k8s/k8s-1_2_1',
-                'release-notes/k8s/k8s-1_2_0',
                 'release-notes/k8s/k8s-previous',
               ],
             },
