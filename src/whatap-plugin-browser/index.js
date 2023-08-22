732eedb563ca242c2ffabc81eee9f429c7d3a163
whatap-docs <kj.im@whatap.io>
2023-08-22 14:07:58 +0900

    [DevOps] 가이드 문서 페이지에 ChannelTalk 버튼 추가 #20

--- src/whatap-plugin-browser/index.js
+++ src/whatap-plugin-browser/index.js
@@ -22,6 +22,13 @@ module.exports = function () {
                             t.parentNode.insertBefore(a, t);
                           })(window, document, 'script', 'https://repo.whatap-browser-agent.io/rum/prod/v1/whatap-browser-agent.js', 'WhatapBrowserAgent', '');`
                     },
+                    {
+                        tagName: 'script',
+                        innerHTML: `(function(){var w=window;if(w.ChannelIO){return w.console.error("ChannelIO script included twice.");}var ch=function(){ch.c(arguments);};ch.q=[];ch.c=function(args){ch.q.push(args);};w.ChannelIO=ch;function l(){if(w.ChannelIOInitialized){return;}w.ChannelIOInitialized=true;var s=document.createElement("script");s.type="text/javascript";s.async=true;s.src="https://cdn.channel.io/plugin/ch-plugin-web.js";var x=document.getElementsByTagName("script")[0];if(x.parentNode){x.parentNode.insertBefore(s,x);}}if(document.readyState==="complete"){l();}else{w.addEventListener("DOMContentLoaded",l);w.addEventListener("load",l);}})();
+                        ChannelIO('boot', {
+                          "pluginKey": "65effdff-6d63-4830-b329-c32aaac3c3d3"
+                        });`
+                    }
                 ],
             };
         },
