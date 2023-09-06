6dcf62318d7a5ffa4a6aee80df42d9f088f721cf
whatap-docs <kj.im@whatap.io>
2023-09-06 15:13:12 +0900

    openapi-call-db 메뉴 오픈

--- sidebars.js
+++ sidebars.js
@@ -4054,7 +4054,7 @@ const sidebars = {
                 'apidoc/openapi-call-server-stat-data',
               ],
             },
-            // 'apidoc/openapi-call-db',
+            'apidoc/openapi-call-db',
             'apidoc/openapi-call-project-stat',
             'apidoc/openapi-call-long-stat',
             'apidoc/openapi-call-short-stat',
