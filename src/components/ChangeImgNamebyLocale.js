69b4fcbf469793c29f8cd22a5178c499a26cc0d0
whatap-docs <kj.im@whatap.io>
2023-09-06 14:50:05 +0900

    ChangeImgNamebyLocale - 이미지가 없을 경우 에러 화면 출력하도록 수정 #31

--- src/components/ChangeImgNamebyLocale.js
+++ src/components/ChangeImgNamebyLocale.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import checkProduct from '@site/src/components/CheckProduct';
+import React, { useState, useEffect } from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
+
 export default function ChangeImgName({img, desc, className}) {
     const {
         i18n: {currentLocale},
     } = useDocusaurusContext();
-    const product = checkProduct();
+    const [isValid, setIsValid] = useState(null);
+    
+    const imageValidator = (path) => {
+        useEffect(() => {
+            if (isValid === null) {
+                const img = new Image();
+                img.src = path;
+                img.onload = () => {
+                    setIsValid(true);
+                }
+                img.onerror = () => {
+                    setIsValid(false);
+                };
+            }
+        }, [path, isValid]);
+    }
+
     let fext = img.substr(img.lastIndexOf('.') + 1);
     let fileName = img.replace('.' + fext, '');
     let imgFilePath;
@@ -15,6 +31,15 @@ export default function ChangeImgName({img, desc, className}) {
     } else {
         imgFilePath = useBaseUrl('/' + currentLocale + '/img/' + fileName + '-' + currentLocale + '.' + fext);
     }
+    imageValidator(imgFilePath);
+    if (isValid === false) {
+        return (
+            <div class='imgError'>
+                Image not found at path: {imgFilePath}
+            </div>
+        );
+    }
+
     function onError(e) {
         if (currentLocale != 'ko') {
             e.target.src = '/' + currentLocale + '/img/' + img;
