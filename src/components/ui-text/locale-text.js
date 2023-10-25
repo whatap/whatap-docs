131221b9c8f3052da8fdc5a4ac0bc4c5858741a8
whatap-docs <kj.im@whatap.io>
2023-10-25 12:00:28 +0900

    update locale-text.js : anchor 속성 추가

--- src/components/ui-text/locale-text.js
+++ src/components/ui-text/locale-text.js
@@ -2,14 +2,14 @@ import React from 'react';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import whatapLocale from './whatap-locale.json';
 
-const ReplacementLocaleText = ({sid, className}) => {
+const ReplacementLocaleText = ({sid, className, anchor}) => {
     const {
         i18n: {currentLocale},
     } = useDocusaurusContext();
     const uiText = whatapLocale[sid][`${currentLocale}`];
 
     return (
-        <span class={className}>{uiText}</span>
+        <span class={className} id={anchor}>{uiText}</span>
     );
 }
 
