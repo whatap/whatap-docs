c6b5e22e4b11df95a2517659a56719ae72a0d4d9
whatap-docs <kj.im@whatap.io>
2023-11-02 13:49:20 +0900

    update theme - EditThisPage

--- src/theme/EditThisPage/index.js
+++ src/theme/EditThisPage/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import Translate from '@docusaurus/Translate';
+import {ThemeClassNames} from '@docusaurus/theme-common';
+import Link from '@docusaurus/Link';
+import IconEdit from '@theme/Icon/Edit';
 import {useDoc} from '@docusaurus/theme-common/internal';
 import Feedback from '../../components/Feedback';
 
-export default function EditThisPageWrapper(props) {
+export default function EditThisPage({editUrl}) {
   const {metadata} = useDoc();
-
   return (
     <>
       <Feedback resource={metadata.unversionedId} />
-      {/* <EditThisPage {...props} /> */}
     </>
+    // <Link to={editUrl} className={ThemeClassNames.common.editThisPage}>
+    //   <IconEdit />
+    //   <Translate
+    //     id="theme.common.editThisPage"
+    //     description="The link label to edit the current page">
+    //     Edit this page
+    //   </Translate>
+    // </Link>
   );
 }
