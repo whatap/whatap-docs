c6f4b01809bd5b6ee4e6df665cbf5ade607ca9d6
whatap-docs <kj.im@whatap.io>
2023-11-15 16:50:27 +0900

    add replaceuitext.js - uitext 자동 교체 스크립트

---
+++ replaceuitext.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+const jsonFile = require('./src/components/ui-text/whatap-locale.json');
+
+const mdxFilepath = path.resolve(__dirname, './docs/common-items/_download-agent.mdx');
+let mdxContent = fs.readFileSync(mdxFilepath, 'utf-8');
+
+function getServiceIdByKeyword(keyword) {
+  var matchedIDs = [];
+  for (let key in jsonFile) {
+    if (jsonFile[key].ko === keyword.trim()) {
+        console.log(key);
+        matchedIDs.push(key);
+    }
+  }
+  return matchedIDs;
+}
+
+const matches = [...mdxContent.matchAll(/\*\*\*(.*?)\*\*\*/g)];
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let index = 0;
+
+function askQuestion() {
+  if (index >= matches.length) {
+    fs.writeFileSync(mdxFilepath, mdxContent); // 변경된 내용을 mdx 파일에 다시 기록
+    rl.close();
+  } else {
+    const keyword = matches[index][1];
+    const serviceIds = getServiceIdByKeyword(keyword);
+    if (serviceIds.length === 1) {
+      mdxContent = mdxContent.replace(`***${keyword}***`, `<Cmdname sid="${serviceIds[0]}" className="uitext" />`);
+      index++;
+      askQuestion();
+    } else if (serviceIds.length > 1) {
+      rl.question(`There are multiple service IDs matching with ***${keyword}***. Please enter the service ID you want to use: `, (serviceId) => {
+        if (serviceIds.includes(serviceId)) {
+          mdxContent = mdxContent.replace(`***${keyword}***`, `<Cmdname sid="${serviceId}" className="uitext" />`);
+        } else {
+          console.log("The provided service ID is not found in the matching set. Please try again.");
+        }
+        index++;
+        askQuestion();
+      });
+    } else {
+      index++;
+      askQuestion();
+    }
+  }
+}
+
+askQuestion();
