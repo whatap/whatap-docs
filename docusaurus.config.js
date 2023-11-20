574486f6cdf4e05d38adad51a43345de0ed18685
whatap-docs <kj.im@whatap.io>
2023-11-20 09:00:07 +0900

    update esbuild-loader module

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -160,6 +160,16 @@ module.exports = Promise.resolve({
   //     },
   //   }),
   // },
+  webpack: {
+    jsLoader: (isServer) => ({
+      loader: require.resolve('esbuild-loader'),
+      options: {
+        loader: 'tsx',
+        format: isServer ? 'cjs' : undefined,
+        target: isServer ? 'node12' : 'es2017',
+      },
+    }),
+  },
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
