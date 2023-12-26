4295185d5712fd27a8db82c7f3c23fdf21d151c8
whatap-docs <kj.im@whatap.io>
2023-12-26 14:05:45 +0900

    update meilisearch

---
+++ src/theme/SearchBar/index.js
@@ -0,0 +1,24 @@
+import React, { useEffect } from 'react';
+import './style.css';
+import 'meilisearch-docsearch/css';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+
+export default function Component() {
+    const {i18n: {currentLocale} } = useDocusaurusContext();
+
+    useEffect(() => {
+            const docsearch = require('meilisearch-docsearch').default
+            const destroy = docsearch({
+            host: 'https://meilsearch.whatap.io',
+            apiKey:
+                'dgoMBc2t6fVzILDGqhh63uBXACGOmJbKNnE_Xv8_Hqk',
+            indexUid: 'whatap',
+            container: '#docsearch',
+            searchParams: { facets: [ "lang" ] },
+        })
+
+        return () => destroy()
+    }, [])
+
+    return <div id="docsearch" />
+}
