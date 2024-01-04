260faeacc156f64f40bc495b13d469a886987847
whatap-docs <kj.im@whatap.io>
2024-01-04 11:03:11 +0900

    update search function

--- src/theme/SearchBar/index.js
+++ src/theme/SearchBar/index.js
@@ -14,7 +14,7 @@ export default function Component() {
                 'dgoMBc2t6fVzILDGqhh63uBXACGOmJbKNnE_Xv8_Hqk',
             indexUid: 'whatap',
             container: '#docsearch',
-            searchParams: { filter: [`lang = ${currentLocale}`], limit: 30, cropLength: 150 },
+            searchParams: { filter: [`lang = ${currentLocale}`], limit: 30, cropLength: 150, matchingStrategy: "all" },
             hotKeys: ["ctrl+k", "k", "/"]
         })
 
