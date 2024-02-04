f44bcae6b8b7dadb4a8a3f6cec6d4b95db1b00c9
whatap-docs <kj.im@whatap.io>
2024-02-05 08:08:02 +0900

    [개발] Python agent v1.5.6 release note

--- sidebars.js
+++ sidebars.js
@@ -3186,14 +3186,15 @@ const sidebars = {
               className: 'new',
               link: {
                 type: 'doc',
-                id: 'release-notes/python/python-1_5_5',
+                id: 'release-notes/python/python-1_5_6',
               },
               items: [
                 {
                   type: 'doc',
-                  id: 'release-notes/python/python-1_5_5',
+                  id: 'release-notes/python/python-1_5_6',
                   className: 'new',
                 },
+                'release-notes/python/python-1_5_5',
                 'release-notes/python/python-1_5_4',
                 'release-notes/python/python-1_5_3',
                 'release-notes/python/python-1_5_2',
@@ -3202,7 +3203,6 @@ const sidebars = {
                 'release-notes/python/python-1_4_8',
                 'release-notes/python/python-1_4_6',
                 'release-notes/python/python-1_4_4',
-                'release-notes/python/python-1_4_3',
                 'release-notes/python/python-previous',
               ],
             },
