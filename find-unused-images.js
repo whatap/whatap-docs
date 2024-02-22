95e124974f97ba820b2264fc6af12ceb03372b40
whatap-docs <kj.im@whatap.io>
2024-02-22 16:54:43 +0900

    update find images script

--- find-unused-images.js
+++ find-unused-images.js
@@ -1,19 +1,16 @@
 const fs = require('fs');
 
-const allImages = fs.readFileSync(imagePathsAll.txt);
-const mdxImages = fs.readFileSync(imagePaths.txt);
+const allImages = fs.readFileSync('imagePathsAll.txt', 'utf-8');
+const mdxImages = fs.readFileSync('imagePaths.txt', 'utf-8');
 
 // 파일 내용을 배열로 변환
 const allArray = allImages.split('\n');
 const mdxArray = mdxImages.split('\n');
 
-// 배열 결합
-const combinedArray = allArray.concat(mdxArray);
+// 두 배열에서 내용을 비교해 중복된 값 제거하기
+const diffArray = allArray.filter(x => !mdxArray.includes(x)).concat(mdxArray.filter(x => !allArray.includes(x)));
 
-// 중복 제거
-const uniqueArray = [...new Set(combinedArray)];
-
-fs.writeFile('unused', '', { flag: 'w' }, function (err) {
+fs.writeFile('unusedImages.txt', diffArray.sort().join('\n'), { flag: 'w' }, function (err) {
     if (err) throw err;
     console.log("It's saved!");
 })
