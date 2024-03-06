7da2a094421e6bbd5a223ab27c85e25c597de731
whatap-docs <kj.im@whatap.io>
2024-03-06 10:37:42 +0900

    SERVICE-861 [개발] Go agent v0.3.2 release note

--- sidebars.js
+++ sidebars.js
@@ -3328,17 +3328,18 @@ const sidebars = {
               label: 'Go',
               collapsible: true,
               collapsed: true,
-              // className: 'new',
+              className: 'new',
               link: {
                 type: 'doc',
-                id: 'release-notes/golang/golang-0_3_1',
+                id: 'release-notes/golang/golang-0_3_2',
               },
               items: [
                 {
                   type: 'doc',
-                  id: 'release-notes/golang/golang-0_3_1',
-                  // className: 'new'
+                  id: 'release-notes/golang/golang-0_3_2',
+                  className: 'new'
                 },
+                'release-notes/golang/golang-0_3_1',
                 'release-notes/golang/golang-0_3_0',
                 'release-notes/golang/golang-0_2_4',
                 'release-notes/golang/golang-0_2_3',
@@ -3347,7 +3348,6 @@ const sidebars = {
                 'release-notes/golang/golang-0_1_13',
                 'release-notes/golang/golang-0_1_12',
                 'release-notes/golang/golang-0_1_11',
-                'release-notes/golang/golang-0_1_10',
                 'release-notes/golang/golang-previous',
               ],
             },
