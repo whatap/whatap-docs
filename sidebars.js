2466b5dd9906bff2c2bea39a399d09690ac6972d
whatap-docs <kj.im@whatap.io>
2024-03-18 17:18:41 +0900

    Redis V2: update images, sidebars

--- sidebars.js
+++ sidebars.js
@@ -1863,7 +1863,7 @@ const sidebars = {
             },
             {
               type: 'category',
-              label: '알림 설정하기',
+              label: '경고 알림',
               collapsible: true,
               collapsed: true,
               link: {
@@ -2568,7 +2568,7 @@ const sidebars = {
             },
             {
               type: 'category',
-              label: '알림 설정하기',
+              label: '경고 알림',
               collapsible: true,
               collapsed: true,
               link: {
