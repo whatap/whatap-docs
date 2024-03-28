ef8fb0a3d451321155bd7299f02e6d1eb49a4df4
whatap-docs <kj.im@whatap.io>
2024-03-28 11:39:43 +0900

    main: SERVICE-939: update preview 2.4.0.a0 release note

--- sidebars.js
+++ sidebars.js
@@ -2883,9 +2883,10 @@ const sidebars = {
       items: [
         {
           type: 'doc',
-          id: 'release-notes/preview/preview-2_3_0x',
+          id: 'release-notes/preview/preview-2_4_0x',
           className: 'new',
         },
+        'release-notes/preview/preview-2_3_0x',
         'release-notes/preview/preview-2_2_0x',
         'release-notes/preview/preview-2_1_0x',
         'release-notes/preview/preview-2_0_0x',
