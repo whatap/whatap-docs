49f32f9674aef91db8b5ee21d46543441be3bf07
ysjo <ysjo@whatap.io>
2024-04-19 15:08:22 +0900

    servcie-1011 changelog 크롤링 스크립트 테스트2

--- crw.js
+++ crw.js
@@ -6,9 +6,11 @@ const cheerio = require('cheerio');
 // 크롤링할 웹 페이지 URL
 const url = 'https://docs.whatap.io/release-notes/service/service-2_3_x';
 
-// 고쳐야 하는 것: feature 기준으로 제품명 가져오기, 리스트 형식 출력 방식 
+// 고쳐야 하는 것:
+// 묶음에 feature 있을 경우, feature 없는 <p> 배열이 출력
+// 반쯤 해결? feature 기준으로 제품명 가져오기, 리스트 형식 출력 방식 
 
-// Axios를 사용하여 웹 페이지 HTML 가져오기5
+// Axios를 사용하여 웹 페이지 HTML 가져오기5-2
 axios.get(url)
     .then(response => {
         // Cheerio를 사용하여 HTML 파싱
@@ -27,8 +29,23 @@ axios.get(url)
                 const version = $(element).find('h2').text().trim();
                 // 릴리스 노트 날짜 가져오기
                 const date = $(element).find('h2 + p').text().trim();
+
+                // 제품명 초기화 
+                let productName = '';
+                // <h3>를 찾아냄
+                const h3Elements = $(element).find('h3');
+                // <h3>가 여러 개인 경우 가장 가까운 <code class="Feature">를 찾음
+                h3Elements.each((index, h3Element) => {
+                const nextElement = $(h3Element).next(); // <h3>의 다음 형제 요소
+
+                // <ul> 또는 <p>에서 <code class="Feature">를 찾음
+                const featureElement = nextElement.find('ul code.Feature, p code.Feature').first();
+
+                productName = $(h3Element).text().trim();
+                });
+
                 // 제품명 가져오기
-                const productName = $(element).find('div.indentTab > h3').text().trim();
+                // const productName = $(element).find('div.indentTab > h3').text().trim();
 
                 // 중복을 제거한 <code class="Feature"> 내용 가져오기
                 const features = new Set();
@@ -52,8 +69,24 @@ axios.get(url)
                     const version = $(element).find('h2').text().trim();
                     // 릴리스 노트 날짜 가져오기
                     const date = $(element).find('h2 + p').text().trim();
+
+                    // 제품명 초기화 
+                    let productName = '';
+                    // <h3>를 찾아냄
+                    const h3Elements = $(element).find('h3');
+                    // <h3>가 여러 개인 경우 가장 가까운 <code class="Feature">를 찾음
+                    h3Elements.each((index, h3Element) => {
+                    const nextElement = $(h3Element).next(); // <h3>의 다음 형제 요소
+
+                    // <ul> 또는 <p>에서 <code class="Feature">를 찾음
+                    const featureElement = nextElement.find('ul code.Feature, p code.Feature').first();
+                    
+                    productName = $(h3Element).text().trim();
+                    });
+
                     // 제품명 가져오기
-                    const productName = $(element).find('div.indentTab > h3').text().trim();
+                    // const productName = $(element).find('div.indentTab > h3').text().trim();
+
                     // <p>의 안에 있는 <code class="Feature"> 태그의 내용 가져오기
                     const featureContent = nextElement.html().trim();
 
