95ee8c5a4c12e8bd92407eb0725151c61174cf48
ysjo <ysjo@whatap.io>
2024-04-26 16:21:31 +0900

    service-1011 에이전트 기능 상세 크롤링1

--- crw-ag.js
+++ crw-ag.js
@@ -9,7 +9,10 @@ const url = 'https://docs.whatap.io/release-notes/java/java-2_2_28';
 // URL을 '/'로 분할하여 배열로 만든 후 배열의 마지막 요소 가져오기
 const segments = url.split('/');
 const lastUrl = segments[segments.length - 1];
-console.log(lastUrl);
+
+const seg2 = lastUrl.split('-')
+const lastUrl2 = seg2[0];
+console.log(lastUrl2);
 
 // Axios를 사용하여 웹 페이지 HTML 가져오기
 axios.get(url)
@@ -24,16 +27,41 @@ axios.get(url)
       // 중복을 제거하기 위한 Set
       let contentSet = new Set();
 
-      
       // 에이전트 버전 정보 가져오기
-      const version = $('div.theme-doc-markdown markdown').find('h1').text().trim();
+      const version = $('*').find('header').text().trim();
       // 날짜 정보 가져오기
-      const date = $(version).next('p').first().text().trim();
+      const date = $('*').find('header + p').text().trim();
 
       // 상세 정보 가져오기
-      const ulContents= $(date).next('ul').text();
+      const ulContents= $('*').find('header + p').next('ul');
 
-      console.log(version + '테스트'); 
+      //console.log(ulContents.length);
+
+      const featureInUl = ulContents.find('code.Feature, code.New');
+
+      const features = new Set();
+
+      featureInUl.each((idx, code) => {
+          features.add($(code).parent().html().trim());
+      });
+
+      // console.log('버전: ' + version + ' 날짜: ' + date + ' 상세: ' + [...features].map(feature => `- ${feature}`).join('\n\n')); 
+
+      // 출력할 필요 없는 경우는 건너뛰기
+      if (features.size === 0) return;
+
+      const content = `#### ${version}` + ` - ${date}`+ `\n`;
+      if (!contentSet.has(content)) {
+          mdxContent += content + '\n';
+          contentSet.add(content);
+      }
+      mdxContent += [...features].map(feature => `- ${feature}`).join('\n\n') +
+          '\n\n';
+
+      // MDX 파일로 데이터 저장
+      const fileName = `./crw-data/_import-${lastUrl2}-agent.mdx`; // 파일 경로 및 이름 설정
+      fs.writeFileSync(fileName, mdxContent);
+      console.log(`MDX file saved: ${fileName}`);
 
     })
     .catch(error => {
