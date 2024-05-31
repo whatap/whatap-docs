610341648c2405ef6ff71a67758bedf1ffedd127
ysjo <ysjo@whatap.io>
2024-05-31 09:26:17 +0900

    service-1156 작업 중

--- CustomList.js
+++ CustomList.js
@@ -1,29 +1,41 @@
-// CustomList.js
 import React from 'react';
-import CustomListItem from './CustomListItem';
 
-function CustomList({ items }) {
-  const uniqueDates = [];
-  
-  return (
-    <ul>
-      {items.map((item, index) => {
-        // 중복된 날짜를 확인하고 처리
-        if (!uniqueDates.includes(item.date)) {
-          uniqueDates.push(item.date);
-          return (
-            <CustomListItem key={index} date={item.date}>
-              {item.content}
-            </CustomListItem>
-          );
-        }
-        return (
-          <CustomListItem key={index} date={item.date} hidden />
-        );
-      })}
-    </ul>
-  );
+// MDX 파일에서 날짜 정보를 추출하는 함수
+function extractDatesFromMDX(mdxContent) {
+    const regex = /<code class='changelog-date'>(.*?)<\/code>/g;
+    const dates = [];
+    let match;
+    while ((match = regex.exec(mdxContent)) !== null) {
+        dates.push(match[1]);
+    }
+    return dates;
+}
+
+// 가장 상단에 위치한 날짜를 식별하는 함수
+function findTopDate(dates) {
+    // 가장 먼저 나타나는 날짜를 반환
+    return dates[0];
+}
+
+// 커스텀 리스트 컴포넌트
+function CustomList({ mdxContent }) {
+    // MDX 파일에서 날짜 정보 추출
+    const dates = extractDatesFromMDX(mdxContent);
+    // 가장 상단에 위치한 날짜 식별
+    const topDate = findTopDate(dates);
+
+    return (
+        <div className="history_overview">
+            <ul>
+                {dates.map((date, index) => (
+                    <li key={index} style={{ display: dates.indexOf(date) === index ? 'block' : 'none' }}>
+                        <span className="changelog-date">{date}</span>
+                        {/* 기타 내용들 */}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 export default CustomList;
-
