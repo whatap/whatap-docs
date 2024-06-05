a51a262b36569848ad95275d8ce1865cc2b596b1
whatap-docs <kj.im@whatap.io>
2024-06-05 15:24:49 +0900

    SERVICE-1232: update mobile app images

--- src/components/ui-text/mobile-locale.js
+++ src/components/ui-text/mobile-locale.js
@@ -4,8 +4,12 @@ import mobileLocale from './mobile-locale.json';
 
 const ReplaceMobileLocaleText = ({sid, className}) => {
     const { i18n: {currentLocale} } = useDocusaurusContext();
-    
-    const uiText = mobileLocale[currentLocale][sid];
+    let uiText;
+    if (sid == "db_instance_list") {
+        uiText = mobileLocale[currentLocale][sid].replace(' （총 @count 개）', '');
+    } else {
+        uiText = mobileLocale[currentLocale][sid];
+    }
 
     return (
         <span class={className}>{uiText}</span>
