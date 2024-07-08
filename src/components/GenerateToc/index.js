2216abdf07b0c50c7b3e447218b85cebba6a152b
whatap-docs <kj.im@whatap.io>
2024-07-08 17:05:39 +0900

    update GenerateToc compnent

---
+++ src/components/GenerateToc/index.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import clsx from 'clsx';
+import TOCItems from '@theme/TOCItems';
+import styles from './styles.module.css';
+
+export default function GenerateTOC({ className, ...props }) {
+  const [tocItems, setTocItems] = useState([]);
+
+  useEffect(() => {
+    // 페이지가 렌더링된 후에 TOC를 생성합니다.
+    const headers = document.querySelectorAll('h2, h3');
+    const collectedToc = Array.from(headers).map(header => ({
+      value: header.innerText,
+      id: header.id,
+      level: parseInt(header.tagName[1], 10),
+    }));
+
+    // TOC 데이터 수집
+    setTocItems(collectedToc);
+  }, []);
+
+  const LINK_CLASS_NAME = 'table-of-contents__link toc-highlight';
+  const LINK_ACTIVE_CLASS_NAME = 'table-of-contents__link--active';
+
+  return (
+    <div className={clsx(styles.tableOfContents, 'thin-scrollbar', className)}>
+      <TOCItems
+        toc={tocItems}
+        linkClassName={LINK_CLASS_NAME}
+        linkActiveClassName={LINK_ACTIVE_CLASS_NAME}
+        {...props}
+      />
+    </div>
+  );
+}
