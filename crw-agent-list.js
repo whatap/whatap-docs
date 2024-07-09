bf8172ff80da5b51e682b97b22014538548c5d35
ysjo <ysjo@whatap.io>
2024-07-09 13:44:55 +0900

    service-1371 새로운 기능 2분기 데이터베이스 에이전트 정리(service-1376)

--- crw-agent-list.js
+++ crw-agent-list.js
@@ -8,9 +8,9 @@ const cheerio = require('cheerio');
 
 // URL 배열 정의
 const urls = [
-    'https://docs.whatap.io/release-notes/k8s/k8s-1_7_0',
-    'https://docs.whatap.io/release-notes/k8s/k8s-1_7_2',
-    'https://docs.whatap.io/release-notes/k8s/k8s-1_7_5',
+    'https://docs.whatap.io/release-notes/db/dbx-1_8_0',
+    'https://docs.whatap.io/release-notes/db/dbx-1_8_1',
+    // 'https://docs.whatap.io/release-notes/db/dbx-1_7_8',
 ];
 
 // 순차적으로 각 URL을 처리하는 함수
