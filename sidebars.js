63151479b67025e2514bdff73c720979be440857
whatap-docs <kj.im@whatap.io>
2024-07-24 08:11:26 +0900

    SERVICE-1451: add K8s agent v1.7.8 release note

--- sidebars.js
+++ sidebars.js
@@ -3272,14 +3272,15 @@ const sidebars = {
               className: 'new',
               link: {
                 type: 'doc',
-                id: 'release-notes/k8s/k8s-1_7_7',
+                id: 'release-notes/k8s/k8s-1_7_8',
               },
               items: [
                 {
                   type: 'doc',
-                  id: 'release-notes/k8s/k8s-1_7_7',
+                  id: 'release-notes/k8s/k8s-1_7_8',
                   className: 'new',
                 },
+                'release-notes/k8s/k8s-1_7_7',
                 'release-notes/k8s/k8s-1_7_6',
                 'release-notes/k8s/k8s-1_7_5',
                 'release-notes/k8s/k8s-1_7_4',
@@ -3289,7 +3290,6 @@ const sidebars = {
                 'release-notes/k8s/k8s-1_7_0',
                 'release-notes/k8s/k8s-1_6_1',
                 'release-notes/k8s/k8s-1_6_0',
-                'release-notes/k8s/k8s-1_5_9',
                 'release-notes/k8s/k8s-previous',
               ],
             },
