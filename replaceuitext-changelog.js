44dce48bcf96843b08fd72c71070e8a36f9adda7
ysjo <ysjo@whatap.io>
2024-07-25 18:35:11 +0900

    service-1468 24q1 server cmd

--- replaceuitext-changelog.js
+++ replaceuitext-changelog.js
@@ -3,7 +3,7 @@ const path = require('path');
 const readline = require('readline');
 const jsonFile = require('./src/components/ui-text/whatap-locale.json');
 
-const mdxFilepath = path.resolve(__dirname, './crw-data/crwld-service/_import-service-Application.mdx');
+const mdxFilepath = path.resolve(__dirname, './crw-data/crwld-service/_import-service-Server.mdx');
 // const mdxFilepath = path.resolve(__dirname, './blog/2024-2q-summary.mdx');
 let mdxContent = fs.readFileSync(mdxFilepath, 'utf-8');
 
