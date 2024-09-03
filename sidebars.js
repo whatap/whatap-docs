63e3b5c21effcbd31b18a9750961698544390726
whatap-docs <kj.im@whatap.io>
2024-09-04 07:48:10 +0900

    SERVICE-1668: update Server agent v2.6.7 release note

--- sidebars.js
+++ sidebars.js
@@ -3831,9 +3831,10 @@ const sidebars = {
               items: [
                 {
                   type: 'doc',
-                  id: 'release-notes/server/server-2_6_6',
+                  id: 'release-notes/server/server-2_6_7',
                   className: 'new',
                 },
+                'release-notes/server/server-2_6_7',
                 'release-notes/server/server-2_6_5',
                 'release-notes/server/server-2_6_4',
                 'release-notes/server/server-2_6_3',
@@ -3844,7 +3845,6 @@ const sidebars = {
                 'release-notes/server/server-2_5_8',
                 'release-notes/server/server-2_5_7',
                 'release-notes/server/server-2_5_5',
-                'release-notes/server/server-2_5_4',
                 'release-notes/server/server-previous',
               ],
             },
