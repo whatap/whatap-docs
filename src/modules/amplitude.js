ead2f20aac5c6b5858021db0663db42988f2160f
whatap-docs <kj.im@whatap.io>
2024-09-06 13:39:00 +0900

    update amplitude

--- src/modules/amplitude.js
+++ src/modules/amplitude.js
@@ -10,13 +10,24 @@ function initAmplitude() {
     });
 }
 
+function trackSearchParam(param) {
+    const eventProperties = {
+        searchString: param,
+    };
+    amplitude.track("searchParam", eventProperties);
+  }
+
 export default (function amplitudeModule() {
     if (!ExecutionEnvironment.canUseDOM) {
         return null;
     }
 
     initAmplitude();
-
+    if (window.location.search) {
+        const param = window.location.search.replace('?whatap%5Bquery%5D=', '');
+        // ?whatap%5Bquery%5D=whatap.conf
+        trackSearchParam(param)
+    }
     return {
         onRouteUpdate() {
 
