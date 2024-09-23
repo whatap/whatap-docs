995a460015df58425671448c4343c0afae3a04fe
whatap-docs <kj.im@whatap.io>
2024-09-23 09:50:43 +0900

    update checkproduct.js

--- src/components/CheckProduct.js
+++ src/components/CheckProduct.js
@@ -51,13 +51,13 @@ export default function CheckProduct() {
     } else if (cProdt === 'tibero') {
         product = '-tibero';
     } else if (cProdt === 'cubrid-v1') {
-        product = '-cubrid';
+        product = '-cubrid-v1';
     } else if (cProdt === 'cubrid') {
         product = '-cubrid';
     } else if (cProdt === 'altibase') {
         product = '-altibase';
     } else if (cProdt === 'altibase-v1') {
-        product = '-altibase';
+        product = '-altibase-v1';
     } else if (cProdt === 'redis') {
         product = '-redis';
     } else if (cProdt === 'mongodb') {
