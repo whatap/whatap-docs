61b40128918c0546e4d8dcead078490c7da45569
ysjo <ysjo@whatap.io>
2024-09-23 14:48:06 +0900

    service-1698 노드 타임라인 신규 작성1

--- sidebars.js
+++ sidebars.js
@@ -6191,6 +6191,7 @@ const sidebars = {
                 'kubernetes/resource-node-list',
                 'kubernetes/node-details',
                 'kubernetes/node-disk-list',
+                'kubernetes/node-timeline',
                 'kubernetes/object-manifest',
                 // 'kubernetes/kubernetes-event',
                 'kubernetes/kube-apiserver-dashboard',
