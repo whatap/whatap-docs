16bf09a4e6f0ef4331a60e6de5fe9cebb71174e5
whatap-docs <kj.im@whatap.io>
2024-09-23 09:59:25 +0900

    update currentDocurl.js for preview

--- src/components/CurrentDocurl.js
+++ src/components/CurrentDocurl.js
@@ -9,9 +9,9 @@ export default function returnPrevVer({children}) {
     const location = useLocation();
     let cProd;
     if (currentLocale == 'ko') {
-        cProd = location.pathname.split("/")[1];
-    } else {
         cProd = location.pathname.split("/")[2];
+    } else {
+        cProd = location.pathname.split("/")[3];
     }
     let rProd = cProd + '-v1';
     let rURL = location.pathname.replace(cProd, rProd);
