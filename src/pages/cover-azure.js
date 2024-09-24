dfd47fa042ba555db2cce90ceb10ba25ca0c1d84
whatap-docs <kj.im@whatap.io>
2024-09-24 10:23:21 +0900

    update cover pages

--- src/pages/cover-azure.js
+++ src/pages/cover-azure.js
@@ -28,7 +28,7 @@ export default function Cover() {
             Azure Monitor
         </h1>
         <p className={styles.date}>
-            release date. 2024.09.02
+            release date. 2024.09.30
         </p>
         <img className={styles.background} src={bgPath} />
       </div>
