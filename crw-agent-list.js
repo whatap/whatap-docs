8f8ef938a566b8ec139275529249fa42de5ca79f
ysjo <ysjo@whatap.io>
2024-09-27 11:07:42 +0900

    service-1582 24q3 에이전트 dotnet(service-1780 service-1788)

--- crw-agent-list.js
+++ crw-agent-list.js
@@ -8,10 +8,11 @@ const cheerio = require('cheerio');
 
 // URL 배열 정의
 const urls = [
-    // 'https://docs.whatap.io/release-notes/nodejs/nodejs-0_4_97',
-    'https://docs.whatap.io/release-notes/python/python-1_6_8',
-    'https://docs.whatap.io/release-notes/python/python-1_6_9',
-    'https://docs.whatap.io/release-notes/python/python-1_6_10',
+    'https://docs.whatap.io/release-notes/dotnet/dotnet-2_2_8',
+    'https://docs.whatap.io/release-notes/dotnet/dotnet-2_2_9',
+    'https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_1',
+    'https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_2',
+    'https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_3',
 ];
 
 // 순차적으로 각 URL을 처리하는 함수
