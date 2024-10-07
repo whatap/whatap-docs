24d0c9361527724cefc60dd65673b3a625bbc5cc
ysjo <ysjo@whatap.io>
2024-10-07 18:15:50 +0900

    service-1813 네비게이션 다국어 옵션 추가(service-1809)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -681,8 +681,21 @@ const config = {
             position: 'left',
             label: 'FAQ',
           },
+          // {
+          //   to: 'blog', 
+          //   label: '새로운 기능', 
+          //   position: 'left', 
+          //   className: 'iflang-link',
+          // },
+          // 다국어 조건 블로그
           {
-            to: 'blog', label: '새로운 기능', position: 'left'
+            to: (function () {
+              const locale = process.env.DOCUSAURUS_CURRENT_LOCALE; // 현재 로케일
+              return locale === 'en' || locale === 'ja' ? 'blog/overview' : 'blog';
+            })(),
+            label: '새로운 기능',
+            position: 'left',
+            className: 'iflang-link',
           },
           {
             type: 'localeDropdown',
