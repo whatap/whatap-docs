8b76657be61541bea19f71cb5b864011d965b4c7
whatap-docs <kj.im@whatap.io>
2024-10-07 09:43:38 +0900

    update package modules

--- src/theme/SearchBar/Canary.jsx
+++ src/theme/SearchBar/Canary.jsx
@@ -4,16 +4,21 @@ export default function Canary({ options }) {
   const [loaded, setLoaded] = React.useState(false);
 
   React.useEffect(() => {
-    Promise.all([
-      import("@getcanary/web/components/canary-root"),
-      import("@getcanary/web/components/canary-provider-pagefind"),
-      import("@getcanary/web/components/canary-modal"),
-      import("@getcanary/web/components/canary-trigger-searchbar"),
-      import("@getcanary/web/components/canary-content"),
-      import("@getcanary/web/components/canary-search"),
-      import("@getcanary/web/components/canary-search-input"),
-      import("@getcanary/web/components/canary-search-results"),
-    ])
+    Promise.all(
+      [
+        import("@getcanary/web/components/canary-root"),
+        import("@getcanary/web/components/canary-provider-pagefind"),
+        import("@getcanary/web/components/canary-modal"),
+        import("@getcanary/web/components/canary-trigger-searchbar"),
+        import("@getcanary/web/components/canary-input"),
+        import("@getcanary/web/components/canary-content"),
+        import("@getcanary/web/components/canary-search"),
+        import("@getcanary/web/components/canary-search-results"),
+        options?.tabs?.length
+          ? import("@getcanary/web/components/canary-filter-tabs-glob")
+          : null,
+      ].filter(Boolean),
+    )
       .then(() => setLoaded(true))
       .catch((e) =>
         console.error("Maybe you forgot to install '@getcanary/web'?", e),
@@ -30,12 +35,15 @@ export default function Canary({ options }) {
         <canary-modal>
           <canary-trigger-searchbar slot="trigger"></canary-trigger-searchbar>
           <canary-content slot="content">
+            <canary-input slot="input"></canary-input>
             <canary-search slot="mode">
-              <canary-search-input slot="input"></canary-search-input>
-              <canary-search-results
-                slot="result"
-                group
-              ></canary-search-results>
+              {options?.tabs?.length ? (
+                <canary-filter-tabs-glob
+                  slot="head"
+                  tabs={JSON.stringify(options.tabs)}
+                ></canary-filter-tabs-glob>
+              ) : null}
+              <canary-search-results slot="body"></canary-search-results>
             </canary-search>
           </canary-content>
         </canary-modal>
--- src/theme/SearchBar/index.jsx
+++ src/theme/SearchBar/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import ErrorBoundary from "@docusaurus/ErrorBoundary";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import { usePluginData } from "@docusaurus/useGlobalData";
 
@@ -8,12 +9,12 @@ import Canary from "./Canary";
 export default function Index() {
   const { siteConfig } = useDocusaurusContext();
   const [path, setPath] = React.useState("");
-  
+
   React.useEffect(() => {
     setPath(`${siteConfig.baseUrl}pagefind/pagefind.js`);
   }, [siteConfig]);
 
-  const { options } = usePluginData("docusaurus-plugin-pagefind-canary");
+  const { options } = usePluginData("docusaurus-theme-search-pagefind");
   const { styles, ...rest } = options;
 
   React.useEffect(() => {
@@ -28,5 +29,34 @@ export default function Index() {
     return null;
   }
 
-  return <Canary options={{ ...rest, path: path }} />;
+  return (
+    <ErrorBoundary
+      fallback={({ error, tryAgain }) => (
+        <div>
+          <p>Canary crashed because: "{error.message}".</p>
+          <p>
+            Most likely, your production build will be fine.{" "}
+            <pre>(docusaurus build && docusaurus serve)</pre>
+          </p>
+          <p>Here's what you can do:</p>
+          <ul>
+            <li>
+              Try to <button onClick={tryAgain}>reload</button> the page.
+            </li>
+            <li>
+              Run production build at least once: <pre>docusaurus build</pre>
+            </li>
+            <li>
+              If the problem persists, please{" "}
+              <a href="https://github.com/fastrepl/canary/issues/new">
+                open an issue.
+              </a>
+            </li>
+          </ul>
+        </div>
+      )}
+    >
+      <Canary options={{ ...rest, path: path }} />
+    </ErrorBoundary>
+  );
 }
