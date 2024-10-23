a3de61bef2466fad1af8fb25acf530695e4c7c79
ysjo <ysjo@whatap.io>
2024-10-23 14:58:56 +0900

    service-1899 서버 에이전트 릴리스 노트

--- sidebars.js
+++ sidebars.js
@@ -4067,12 +4067,17 @@ const sidebars = {
               label: 'Server AIX',
               collapsible: true,
               collapsed: true,
-              // className: 'new',
+              className: 'new',
               link: {
                 type: 'doc',
                 id: 'release-notes/server-aix/index'
               },
               items: [
+                {
+                  type: 'doc',
+                  id: 'release-notes/server-aix/server-aix-1_3_5',
+                  className: 'new'
+                },
                 {
                   type: 'doc',
                   id: 'release-notes/server-aix/server-aix-1_3_4',
@@ -4082,10 +4087,23 @@ const sidebars = {
               ],
             },
             {
-              type: 'doc',
+              type: 'category',
               label: 'Server Solaris',
-              // className: 'new',
-              id: 'release-notes/server-solaris/server-solaris-1_3_4'
+              collapsible: true,
+              collapsed: true,
+              className: 'new',
+              link: {
+                type: 'doc',
+                id: 'release-notes/server-solaris/server-solaris-1_3_5'
+              },
+              items: [
+                {
+                  type: 'doc',
+                  id: 'release-notes/server-solaris/server-solaris-1_3_5',
+                  className: 'new'
+                },
+                'release-notes/server-solaris/server-solaris-1_3_4',
+              ],
             },
             {
               type: 'doc',
