6d069d487167989a17f149bac053e92ba650b8b5
whatap-docs <kj.im@whatap.io>
2024-10-31 10:21:17 +0900

    SERVICE-1938: update release notes pdf files, pdflist.js

--- batch-generate-release-notes.sh
+++ batch-generate-release-notes.sh
@@ -2,81 +2,93 @@
 PATH_NAME=$(pwd)
 echo $PATH_NAME
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/service/service-2_8_x --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/service-2.8.x.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/service/service-2_9_x --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/service-2.9.x.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/service-2.8.x.pdf
+pdfcpu annot remove -pages 1 ./pdf/service-2.9.x.pdf
 
-# npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/java/java-2_2_38 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/java-agent-v2.2.38.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/java/java-2_2_39 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/java-agent-v2.2.39.pdf --dest ./pdf --include-index --selector 'none'
 
-# pdfcpu annot remove -pages 1 ./pdf/java-agent-v2.2.38.pdf
+pdfcpu annot remove -pages 1 ./pdf/java-agent-v2.2.39.pdf
 
 # npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/java-batch/java-batch-2_2_25 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/java-batch-agent-v2.2.25.pdf --dest ./pdf --include-index --selector 'none'
 
 # pdfcpu annot remove -pages 1 ./pdf/java-batch-agent-v2.2.25.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/php/php-2_8_2 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/php-agent-v2.8.2.pdf --dest ./pdf --include-index --selector 'none'
+# npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/php/php-2_8_2 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/php-agent-v2.8.2.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/php-agent-v2.8.2.pdf
+# pdfcpu annot remove -pages 1 ./pdf/php-agent-v2.8.2.pdf
 
-# npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/nodejs/nodejs-0_5_1 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/nodejs-agent-v0.5.1.pdf --dsest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/nodejs/nodejs-0_5_2 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/nodejs-agent-v0.5.2.pdf --dsest ./pdf --include-index --selector 'none'
 
-# pdfcpu annot remove -pages 1 ./pdf/nodejs-agent-v0.5.1.pdf
+pdfcpu annot remove -pages 1 ./pdf/nodejs-agent-v0.5.2.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/python/python-1_7_0 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/python-agent-v1.7.0.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/python/python-1_7_1 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/python-agent-v1.7.1.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/python-agent-v1.7.0.pdf
+pdfcpu annot remove -pages 1 ./pdf/python-agent-v1.7.1.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/python/python-1_6_10 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/python-agent-v1.6.10.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_4 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dotnet-agent-v2.3.4.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/python-agent-v1.6.10.pdf
-
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/python/python-1_6_9 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/python-agent-v1.6.9.pdf --dest ./pdf --include-index --selector 'none'
-
-pdfcpu annot remove -pages 1 ./pdf/python-agent-v1.6.9.pdf
+pdfcpu annot remove -pages 1 ./pdf/dotnet-agent-v2.3.4.pdf
 
 npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_3 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dotnet-agent-v2.3.3.pdf --dest ./pdf --include-index --selector 'none'
 
 pdfcpu annot remove -pages 1 ./pdf/dotnet-agent-v2.3.3.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/dotnet/dotnet-2_3_2 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dotnet-agent-v2.3.2.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/golang/golang-0_4_3 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/go-agent-v0.4.3.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/dotnet-agent-v2.3.2.pdf
+pdfcpu annot remove -pages 1 ./pdf/go-agent-v0.4.3.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/golang/golang-0_4_2 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/go-agent-v0.4.2.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server/server-2_7_0 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-agent-v2.7.0.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/go-agent-v0.4.2.pdf
+pdfcpu annot remove -pages 1 ./pdf/server-agent-v2.7.0.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server/server-2_6_8 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-agent-v2.6.8.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server/server-2_6_9 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-agent-v2.6.9.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/server-agent-v2.6.8.pdf
+pdfcpu annot remove -pages 1 ./pdf/server-agent-v2.6.9.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server/server-2_6_7 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-agent-v2.6.7.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server-aix/server-aix-1_3_5 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-aix-agent-v1.3.5.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/server-agent-v2.6.7.pdf
+pdfcpu annot remove -pages 1 ./pdf/server-aix-agent-v1.3.5.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server-aix/server-aix-1_3_4 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-aix-agent-v1.3.4.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server-solaris/server-solaris-1_3_5 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-solaris-agent-v1.3.5.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/server-aix-agent-v1.3.4.pdf
-
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server-solaris/server-solaris-1_3_4 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-solaris-agent-v1.3.4.pdf --dest ./pdf --include-index --selector 'none'
-
-pdfcpu annot remove -pages 1 ./pdf/server-solaris-agent-v1.3.4.pdf
+pdfcpu annot remove -pages 1 ./pdf/server-solaris-agent-v1.3.5.pdf
 
 npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/server-hpux/server-hpux-1_3_4 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/server-hpux-agent-v1.3.4.pdf --dest ./pdf --include-index --selector 'none'
 
 pdfcpu annot remove -pages 1 ./pdf/server-hpux-agent-v1.3.4.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/k8s/k8s-1_7_11 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/k8s-agent-v1.7.11.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/k8s/k8s-1_7_14 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/k8s-agent-v1.7.14.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/k8s-agent-v1.7.11.pdf
+pdfcpu annot remove -pages 1 ./pdf/k8s-agent-v1.7.14.pdf
 
-npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/k8s/k8s-1_7_10 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/k8s-agent-v1.7.10.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/k8s/k8s-1_7_13 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/k8s-agent-v1.7.13.pdf --dest ./pdf --include-index --selector 'none'
 
-pdfcpu annot remove -pages 1 ./pdf/k8s-agent-v1.7.10.pdf
+pdfcpu annot remove -pages 1 ./pdf/k8s-agent-v1.7.13.pdf
 
-# npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/db/dbx-2_0_0 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dbx-agent-v2.0.0.pdf --dest ./pdf --include-index --selector 'none'
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/k8s/k8s-1_7_12 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/k8s-agent-v1.7.12.pdf --dest ./pdf --include-index --selector 'none'
 
-# pdfcpu annot remove -pages 1 ./pdf/dbx-agent-v2.0.0.pdf
+pdfcpu annot remove -pages 1 ./pdf/k8s-agent-v1.7.12.pdf
+
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/db/dbx-2_1_2 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dbx-agent-v2.1.2.pdf --dest ./pdf --include-index --selector 'none'
+
+pdfcpu annot remove -pages 1 ./pdf/dbx-agent-v2.1.2.pdf
+
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/db/dbx-2_1_1 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dbx-agent-v2.1.1.pdf --dest ./pdf --include-index --selector 'none'
+
+pdfcpu annot remove -pages 1 ./pdf/dbx-agent-v2.1.1.pdf
+
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/db/dbx-2_1_0 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dbx-agent-v2.1.0.pdf --dest ./pdf --include-index --selector 'none'
+
+pdfcpu annot remove -pages 1 ./pdf/dbx-agent-v2.1.0.pdf
+
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/db/dbx-2_0_2 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dbx-agent-v2.0.2.pdf --dest ./pdf --include-index --selector 'none'
+
+pdfcpu annot remove -pages 1 ./pdf/dbx-agent-v2.0.2.pdf
+
+npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/db/dbx-2_0_1 --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/dbx-agent-v2.0.1.pdf --dest ./pdf --include-index --selector 'none'
+
+pdfcpu annot remove -pages 1 ./pdf/dbx-agent-v2.0.1.pdf
 
 # npx docusaurus-prince-pdf -u https://docs.whatap.io/release-notes/db/xos-1_1_8f --prince-args="--javascript --page-size='a4' --page-margin='10mm' --style='$PATH_NAME/print.css'" -o ./pdf/xos-agent-v1.1.8f.pdf --dest ./pdf --include-index --selector 'none'
 
