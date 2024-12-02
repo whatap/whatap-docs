ffef8d4e778bd6456d26680c44e6ec1f9f38de31
Imkijung <fneathe@gmail.com>
2024-12-02 22:29:05 +0900

    update Styles.jsx

--- src/theme/Redoc/Styles.jsx
+++ src/theme/Redoc/Styles.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 import '../../global';
-export function ServerStyles(_props) {
-    return (
+
+export function ServerStyles(_props: {
+  specProps: SpecProps;
+  lightThemeOptions: RedocRawOptions;
+  darkThemeOptions: RedocRawOptions;
+}) {
+  return (
     <div
       className="redocusaurus-styles"
       dangerouslySetInnerHTML={{ __html: '' }}
