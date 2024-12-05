a8b5a43e9b117dea39ad26856012e7d8bd735f60
whatap-docs <kj.im@whatap.io>
2024-12-06 07:47:58 +0900

    update css, Footer Layout theme

---
+++ src/theme/Footer/Layout/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import clsx from 'clsx';
+import {useLocation} from '@docusaurus/router';
+export default function FooterLayout({style, links, logo, copyright}) {
+  const location = useLocation();
+  const isOpenapi = location.pathname.includes("openapi-spec");
+  return (
+    <footer
+      className={clsx('footer', {
+        'footer--dark': style === 'dark',
+      },
+      isOpenapi ? "full--width" : null,
+      )}>
+      <div className="container container-fluid">
+        {links}
+        {(logo || copyright) && (
+          <div className="footer__bottom text--center">
+            {logo && <div className="margin-bottom--sm">{logo}</div>}
+            {copyright}
+          </div>
+        )}
+      </div>
+    </footer>
+  );
+}
