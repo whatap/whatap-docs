7980fad7ca3b0c7831cb34a6ed157cfc044c9c97
whatap-docs <kj.im@whatap.io>
2024-12-12 11:41:48 +0900

    update index.js: 검색 결과 목록의 포커스된 아이템에서 Enter를 입력하면 해당 주소로 이동하도록 수정

--- src/pages/index.js
+++ src/pages/index.js
@@ -66,6 +66,18 @@ function App() {
                         window.location.href = searchUrl;
                     }
                 }}
+                onStateChange={(state) => {
+                    // 검색 결과 목록에서 Enter를 입력하면 해당 item으로 이동하는 코드
+                    const curid = state.state.activeItemId !== null ? state.state.activeItemId : false;
+                    if (curid !== false) {
+                        const activeUrl = state.state.collections[0].items[curid].url;
+                        window.onkeydown = (e) => {
+                            if (e.keyCode === 13) {
+                                window.location.href = activeUrl;
+                            }
+                        }
+                    }
+                }}
             />
         </div>
     );
