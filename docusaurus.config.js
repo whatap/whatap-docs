3adbbca6cbd4f1314a8e96d5ec35a776e891c556
whatap-docs <kj.im@whatap.io>
2025-01-06 10:51:25 +0900

    update config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -218,7 +218,7 @@ const config = {
         },
         {
           name: 'google-site-verification',
-          content: 'rIlfJkbbogFOIDc3DgJZmOGbUMJWytyvZSKEgV7EMsE',
+          content: 'nO_wrAffJ4zPi5ITqOMVr2q7i9lX_EdEi-T2EPv2jKs',
         }
       ],
       blog: {
