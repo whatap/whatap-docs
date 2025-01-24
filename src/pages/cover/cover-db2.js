a433883b6ac42da56f71d5a3690b69fa10cd9f92
whatap-docs <kj.im@whatap.io>
2025-01-24 14:09:18 +0900

    update cover-db2.js

--- src/pages/cover/cover-db2.js
+++ src/pages/cover/cover-db2.js
@@ -6,7 +6,7 @@ import Head from '@docusaurus/Head';
 
 export default function Cover() {
   const bgPath = useBaseUrl('/img/cover-background.png');
-  const cover = useBaseUrl('/mssql/monitoring-intro');
+  const cover = useBaseUrl('/db2/monitoring-intro');
   return (
     <Layout>
     <Head>
