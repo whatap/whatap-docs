e8174a255c57906c7b076630dd762410638323ab
whatap-docs <kj.im@whatap.io>
2025-02-03 09:33:33 +0900

    update breadcrumbs theme

--- src/theme/DocBreadcrumbs/index.js
+++ src/theme/DocBreadcrumbs/index.js
@@ -58,7 +58,7 @@ function BreadcrumbsItem({children, active, index, addMicrodata}) {
       })}
       className={clsx('breadcrumbs__item', {
         'breadcrumbs__item--active': active,
-      })}>
+      }, String(index + 1))}>
       {children}
       <meta itemProp="position" content={String(index + 1)} />
     </li>
