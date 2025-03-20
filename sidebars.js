f73bb6594a5be9d9d442344b7c9d4b45bcb86570
ysjo <ysjo@whatap.io>
2025-03-20 12:09:30 +0900

    service-2562 k8s 사이드 메뉴 구조 개편1

--- sidebars.js
+++ sidebars.js
@@ -7014,15 +7014,6 @@ const sidebars = {
           items: [
             'kubernetes/cluster-summary',
             'kubernetes/resource-namespace',
-            'kubernetes/object-manifest',
-            'kubernetes/kubernetes-event',
-            'kubernetes/kube-apiserver-dashboard',
-            'kubernetes/kube-apiserver-metrics-search',
-            'kubernetes/etcd-dashboard',
-            'kubernetes/kube-scheduler-dashboard',
-            'kubernetes/service-list',
-            'kubernetes/pv-list',
-            'kubernetes/ingress-list',
           ],
         },
         {
@@ -7061,37 +7052,47 @@ const sidebars = {
             'kubernetes/pending-pod-status',
             'kubernetes/resource-application-list',
             'kubernetes/deployment-list',
-            // 'kubernetes/service-list',
           ],
         },
         {
           type: 'category',
-          label: '클러스터',
+          label: '서비스 네트워킹',
           collapsible: true,
           collapsed: true,
           items: [
-            'kubernetes/resource-node-list',
-            'kubernetes/node-details',
-            'kubernetes/node-disk-list',
-            'kubernetes/node-timeline',
-            'kubernetes/object-manifest',
-            'kubernetes/kubernetes-event',
+            'kubernetes/service-list',
+            'kubernetes/ingress-list',
+          ]
+        },
+        {
+          type: 'category',
+          label: '스토리지',
+          collapsible: true,
+          collapsed: true,
+          items: [
+            'kubernetes/pv-list',
+          ]
+        },
+        {
+          type: 'category',
+          label: '컨트롤 플레인',
+          collapsible: true,
+          collapsed: true,
+          items: [
             'kubernetes/kube-apiserver-dashboard',
             'kubernetes/kube-apiserver-metrics-search',
             'kubernetes/etcd-dashboard',
             'kubernetes/kube-scheduler-dashboard',
-            'kubernetes/service-list',
-            'kubernetes/pv-list',
-            'kubernetes/ingress-list',
-            'kubernetes/resource-namespace',
-          ],
-        },
+          ]
+        }, 
         {
           type: 'category',
           label: '분석',
           collapsible: true,
           collapsed: true,
           items: [
+            'kubernetes/object-manifest',
+            'kubernetes/kubernetes-event',
             {
               type: 'doc',
               id: 'kubernetes/pod-init-perform-v2',
