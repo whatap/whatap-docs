ef9c915024bf57ed1c2e1b81a50ef8c13cc3d986
ysjo <ysjo@whatap.io>
2025-03-27 16:07:50 +0900

    service-2605 서비스 2.16.0 1

--- sidebars.js
+++ sidebars.js
@@ -4607,9 +4607,10 @@ const sidebars = {
           items: [
             {
               type: 'doc',
-              id: 'release-notes/service/service-2_15_x',
+              id: 'release-notes/service/service-2_16_x',
               className: 'new',
             },
+            'release-notes/service/service-2_15_x',
             'release-notes/service/service-2_14_x',
             'release-notes/service/service-2_13_x',
             'release-notes/service/service-2_12_x',
