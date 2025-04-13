c85b81a4cbbb48e80aeaa6615a1b89fb336c47a2
emoh <emoh@whatap.io>
2025-04-13 20:23:22 +0900

    update gtm code

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -78,6 +78,17 @@ const config = {
       },
     },
   ],
+  // google tag manager datalayer script
+  scripts: [
+    {
+      content: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-KT747XQX');`,
+      async: true,
+    },
+  ],
   // trailingSlash: false,
   clientModules: [
     // require.resolve("./src/modules/amplitude.js")
