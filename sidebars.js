a4d20a7ca1a18287153e2fd3451da83cf48a818b
emoh <emoh@whatap.io>
2025-05-19 16:27:10 +0900

    update sidebar

--- sidebars.js
+++ sidebars.js
@@ -4609,10 +4609,9 @@ const sidebars = {
           items: [
             {
               type: 'doc',
-              id: 'release-notes/service/service-2_17_x',
+              id: 'release-notes/service/service-2_16_x',
               className: 'new',
             },
-            'release-notes/service/service-2_16_x',
             'release-notes/service/service-2_15_x',
             'release-notes/service/service-2_14_x',
             'release-notes/service/service-2_13_x',
