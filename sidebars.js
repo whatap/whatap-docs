1aa9716bf750eb9f6d01fba19bf3230633879e88
Park Subin <subin.park@whatap.io>
2025-06-16 15:02:10 +0900

    update docs

--- sidebars.js
+++ sidebars.js
@@ -4638,9 +4638,10 @@ const sidebars = {
           items: [
             {
               type: 'doc',
-              id: 'release-notes/service/service-2_17_x',
+              id: 'release-notes/service/service-2_18_x',
               className: 'new',
             },
+            'release-notes/service/service-2_17_x',
             'release-notes/service/service-2_16_x',
             'release-notes/service/service-2_15_x',
             'release-notes/service/service-2_14_x',
