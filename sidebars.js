9aeafb1aa88b9f5df7066f1ce3a7959bbcce1464
Park Subin <subin.park@whatap.io>
2025-06-23 13:29:49 +0900

    SERVICE-2691 서버 모니터링 > 서버 목록 > gpu 내용 추가

--- sidebars.js
+++ sidebars.js
@@ -6895,6 +6895,8 @@ const sidebars = {
             'server/server-list',
             'server/server-detail',
             'server/server-detail-process-group',
+            'server/gpu-inventory',
+            'server/gpu-performance-summary',
           ]
         },
         // 'server/server-list',
