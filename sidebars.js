fdb8981488d7d5d1dbce80c529704de9b58c4b98
Park Subin <subin.park@whatap.io>
2025-11-10 13:48:53 +0900

    SERVICE-2972 Java Agent v2.2.66 추가

--- sidebars.js
+++ sidebars.js
@@ -4240,7 +4240,7 @@ const sidebars = {
               label: 'Java',
               collapsible: true,
               collapsed: true,
-              // className: 'new',
+              className: 'new',
               link: {
                 type: 'doc',
                 id: 'release-notes/java/index',
@@ -4248,9 +4248,10 @@ const sidebars = {
               items: [
                 {
                   type: 'doc',
-                  id: 'release-notes/java/java-2_2_65',
-                  // className: 'new',
+                  id: 'release-notes/java/java-2_2_66',
+                  className: 'new',
                 },
+                'release-notes/java/java-2_2_65',
                 'release-notes/java/java-2_2_64',
                 'release-notes/java/java-2_2_63',
                 'release-notes/java/java-2_2_62',
