6da04b992778ed4811171c0682cd3ddee37d64fe
Park Subin <subin.park@whatap.io>
2025-11-10 11:12:33 +0900

    SERVICE-2972 Kubernetes Agent v1.8.9 추가

--- sidebars.js
+++ sidebars.js
@@ -4464,7 +4464,7 @@ const sidebars = {
               label: 'Kubernetes',
               collapsible: true,
               collapsed: true,
-              //className: 'new',
+              className: 'new',
               link: {
                 type: 'doc',
                 id: 'release-notes/k8s/index',
@@ -4472,9 +4472,10 @@ const sidebars = {
               items: [
                 {
                   type: 'doc',
-                  id: 'release-notes/k8s/k8s-1_8_8',
-                  // className: 'new',
+                  id: 'release-notes/k8s/k8s-1_8_9',
+                  className: 'new',
                 },
+                'release-notes/k8s/k8s-1_8_8',
                 'release-notes/k8s/k8s-1_8_7',
                 'release-notes/k8s/k8s-1_8_6',
                 'release-notes/k8s/k8s-1_8_5',
